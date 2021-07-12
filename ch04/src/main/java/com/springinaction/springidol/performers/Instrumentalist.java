e83e517c7cd126c6572b16a1744c9357b6392cba
Луков Александр Вячеславович <Lukov.A.V@sberbank.ru>
2021-07-12 17:23:28 +0300

    4.2.1. Использование аннотации @Autowired

--- ch04/src/main/java/com/springinaction/springidol/performers/Instrumentalist.java
+++ ch04/src/main/java/com/springinaction/springidol/performers/Instrumentalist.java
@@ -2,22 +2,31 @@ package com.springinaction.springidol.performers;
 
 import com.springinaction.springidol.exceptions.PerformanceException;
 import com.springinaction.springidol.instruments.Instrument;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Instrumentalist implements Performer {
 
     private String song;
+    @Autowired//Аннотировать можно свойства (поля) и вообще избавиться от методов записи
     private Instrument instrument;
 
     public Instrumentalist() {
     }
 
+    /*@Autowired
+    public Instrumentalist(Instrument instrument) {
+        this.instrument = instrument;
+    }*/
+
     public void setSong(String song) { // Внедрение мелодии
         this.song = song;
     }
 
+    //Аннотировать можно свойства (поля) и вообще избавиться от методов записи
+    /*@Autowired
     public void setInstrument(Instrument instrument) { // Внедрение инструмента
         this.instrument = instrument;
-    }
+    }*/
 
     public void perform() throws PerformanceException {
         System.out.print("Playing " + song + " : ");
