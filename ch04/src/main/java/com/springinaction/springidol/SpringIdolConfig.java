2156366ac5b2fdbaa93e8ac3ad938be31b10a38c
Луков Александр Вячеславович <Lukov.A.V@sberbank.ru>
2021-07-16 12:24:11 +0300

    4.4.4. Внедрение зависимостей в конфигурации на языке Java

--- ch04/src/main/java/com/springinaction/springidol/SpringIdolConfig.java
+++ ch04/src/main/java/com/springinaction/springidol/SpringIdolConfig.java
@@ -1,7 +1,11 @@
 package com.springinaction.springidol;
 
+import com.springinaction.springidol.performers.Instrumentalist;
 import com.springinaction.springidol.performers.Juggler;
 import com.springinaction.springidol.performers.Performer;
+import com.springinaction.springidol.performers.PoeticJuggler;
+import com.springinaction.springidol.poems.Poem;
+import com.springinaction.springidol.poems.Sonnet29;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,5 +33,30 @@ public class SpringIdolConfig {
 
         Метод duke создает и возвращает экземпляр класса Juggler.
         Этот объект будет зарегистрирован фреймворком Spring в контексте приложения с идентификатором duke.*/
+    }
+
+    @Bean
+    public Performer duke15() {
+        return new Juggler(15);
+        //Внедрение значений в компонент
+    }
+
+    @Bean
+    public Performer kenny() {
+        Instrumentalist kenny = new Instrumentalist();
+        kenny.setSong("Jingle Bells");
+        return kenny;
+        //Внедрение через метод записи
+    }
+
+    @Bean
+    Poem sonnet29() {
+        return new Sonnet29();
+    }
+
+    @Bean
+    public Performer poeticDuke() {
+        return new PoeticJuggler(sonnet29());
+        //Внедрение ссылки на другой компонент
     }
 }
