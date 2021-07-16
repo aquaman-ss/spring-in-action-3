a3fc9f87e8ff1209fd8abf8c6a0a760eacfde1e6
Луков Александр Вячеславович <Lukov.A.V@sberbank.ru>
2021-07-16 12:03:36 +0300

    4.4.3. Объявление простого компонента

--- ch04/src/main/java/com/springinaction/springidol/SpringIdolConfig.java
+++ ch04/src/main/java/com/springinaction/springidol/SpringIdolConfig.java
@@ -1,5 +1,8 @@
 package com.springinaction.springidol;
 
+import com.springinaction.springidol.performers.Juggler;
+import com.springinaction.springidol.performers.Performer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,4 +16,18 @@ public class SpringIdolConfig {
     что данный класс содержит одно или более определений компонентов.
 
     Объявления компонентов – это обычные методы, отмеченные аннотацией @Bean.*/
+
+    @Bean
+    public Performer duke() {
+        return new Juggler();
+        /*Эквивалент элемента <bean>
+
+        Аннотация @Bean сообщает фреймворку Spring, что данный метод вернет объект,
+        который должен быть зарегистрирован в контексте приложения Spring как компонент.
+
+        Компонент получит идентификатор, совпадающий с именем метода.
+
+        Метод duke создает и возвращает экземпляр класса Juggler.
+        Этот объект будет зарегистрирован фреймворком Spring в контексте приложения с идентификатором duke.*/
+    }
 }
